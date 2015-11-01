6d56ce9bed9df71b95d78fe70d3aca82d30b1b78
Erik Brakke <eriksbrakke@gmail.com>
2015-10-31 22:06:44 -0400

    Fixed styles on stubbed data

--- api/routes/transactions.js
+++ api/routes/transactions.js
@@ -4,38 +4,41 @@ var authorize = require('../middlewares/authorize');
 var transactions = express.Router();
 
 transactions.get('/:id', authorize.auth, function(req, res, next) {
-    var stubbedData = { name : 'Rick Sanchez',
-                        email : 'wubbalubbadubdub@earth.com',
-                        userID : '00000001',
-                        phoneNum : 1111111111,
-                        accepting : true,
-                        amount : 100,
-                        credit: 30,
-                        location : {
-                            latitude : 100,
-                            longitude : 23 } };
+    var stubbedData = {
+        name : 'Rick Sanchez',
+        email : 'wubbalubbadubdub@earth.com',
+        userID : '00000001',
+        phoneNum : 1111111111,
+        accepting : true,
+        amount : 100,
+        credit: 30,
+        location : {
+            latitude : 100,
+            longitude : 23}
+    };
     res.locals.data = stubbedData;
     next();
 }, envelope);
 
 transactions.post('/', authorize.auth, function(req, res, next) {
-    var stubbedData = { receiver : '00000002',
-                        fulfiller : '00000001',
-                        amount : 40,
-                        status : 'pending'
-                        }; /* HERE */
+    var stubbedData = {
+        receiver : '00000002',
+        fulfiller : '00000001',
+        amount : 40,
+        status : 'pending'
+    };
     res.locals.data = stubbedData;
     next();
 }, envelope);
 
 transactions.post('/:id/verify', authorize.auth, function(req, res, next) {
-    var stubbedData = { message : 'success' }; /* HERE */
+    var stubbedData = {message : 'success'};
     res.locals.data = stubbedData;
     next();
 }, envelope);
 
 transactions.delete('/:id', authorize.auth, function(req, res, next) {
-    var stubbedData = { message : 'success'}; /* HERE */
+    var stubbedData = {message : 'success'};
     res.locals.data = stubbedData;
     next();
 }, envelope);
