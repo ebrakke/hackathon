db81ac9f417cb4b8a89ed4eef3cbf34d9ae24f01
m-marcin <mmarcin@Michaels-MacBook-Pro.local>
2015-10-31 23:16:37 -0400

    dbConnector

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var envelope = require('../middlewares/envelope');
 var index = express.Router();
+var db = require('../helper/dbConnector');
 
 index.get('/', function(req, res, next) {
     res.locals.data = {msg: 'Hello cruel World!'};
