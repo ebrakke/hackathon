793afad49a94c84330c3408636e2b90c79cb5696
kendlera <kendlera@gmail.com>
2015-10-31 22:01:31 -0400

    Added Stud Data

--- api/routes/transactions.js
+++ api/routes/transactions.js
@@ -4,25 +4,38 @@ var authorize = require('../middlewares/authorize');
 var transactions = express.Router();
 
 transactions.get('/:id', authorize.auth, function(req, res, next) {
-    var stubbedData = {}; /* HERE */
+    var stubbedData = { name : 'Rick Sanchez',
+                        email : 'wubbalubbadubdub@earth.com',
+                        userID : '00000001',
+                        phoneNum : 1111111111,
+                        accepting : true,
+                        amount : 100,
+                        credit: 30,
+                        location : {
+                            latitude : 100,
+                            longitude : 23 } };
     res.locals.data = stubbedData;
     next();
 }, envelope);
 
 transactions.post('/', authorize.auth, function(req, res, next) {
-    var stubbedData = {}; /* HERE */
+    var stubbedData = { receiver : '00000002',
+                        fulfiller : '00000001',
+                        amount : 40,
+                        status : 'pending'
+                        }; /* HERE */
     res.locals.data = stubbedData;
     next();
 }, envelope);
 
 transactions.post('/:id/verify', authorize.auth, function(req, res, next) {
-    var stubbedData = {}; /* HERE */
+    var stubbedData = { message : 'success' }; /* HERE */
     res.locals.data = stubbedData;
     next();
 }, envelope);
 
 transactions.delete('/:id', authorize.auth, function(req, res, next) {
-    var stubbedData = {}; /* HERE */
+    var stubbedData = { message : 'success'}; /* HERE */
     res.locals.data = stubbedData;
     next();
 }, envelope);
